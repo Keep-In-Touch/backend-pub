8b7e0068000c28d33629656364fa600608409488
solo1 <a.solovyov@innopolis.ru>
2021-02-26 22:54:05 +0300

    database fix

--- backend/settings.py
+++ backend/settings.py
@@ -95,22 +95,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": env_config.get("SQL_ENGINE", ),
-#         "NAME": env_config.get("SQL_DATABASE", ),
-#         "USER": env_config.get("SQL_USER", ),
-#         "PASSWORD": env_config.get("SQL_PASSWORD", ),
-#         "HOST": env_config.get("SQL_HOST", ),
-#         "PORT": env_config.get("SQL_PORT", ),
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": env_config.get("SQL_ENGINE", ),
+        "NAME": env_config.get("SQL_DATABASE", ),
+        "USER": env_config.get("SQL_USER", ),
+        "PASSWORD": env_config.get("SQL_PASSWORD", ),
+        "HOST": env_config.get("SQL_HOST", ),
+        "PORT": env_config.get("SQL_PORT", ),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
