b4a60efa352bc8197a401507f2bf9287c1417509
solo1 <a.solovyov@innopolis.ru>
2021-03-25 01:31:30 +0300

    pytest configured

--- backend/settings/main.py
+++ backend/settings/main.py
@@ -19,7 +19,7 @@ from decouple import Config, RepositoryEnv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from firebase_admin import credentials
 
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
